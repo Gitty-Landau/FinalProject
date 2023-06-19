3055e969c942d5bb3daf70e9c7e0063b77eeb85f
Gitty Landau <GittyLandau04@gmail.com>
2023-06-19 15:56:16 +0300

    .map

--- finalproj/src/Components/Dashboard/MainDashboard/BCLeft/History/History.js
+++ finalproj/src/Components/Dashboard/MainDashboard/BCLeft/History/History.js
@@ -2,21 +2,23 @@ import "./History.css";
 import Button from "../../../Button/Button";
 import Trow from "./Trow/Trow";
 function History(props) {
+  console.log(Array.isArray(props.tableHeaderArr));
+
   return (
     <div class="box transaction-box">
       <div class="header-container">
         <h3 class="section-header">{props.HeaderText}</h3>
       </div>
+
       <table class="transaction-history">
         <tr>
-          <th>{props.tableHeaderArr[0]}</th>
-          <th>{props.tableHeaderArr[1]}</th>
-          <th>{props.tableHeaderArr[2]}</th>
-          <th>{props.tableHeaderArr[3]}</th>
+          {props.tableHeaderArr.map(function (header) {
+            return <th>{header}</th>;
+          })}
         </tr>
-        <Trow obj={props.donationsArr[0]}></Trow>
-        <Trow obj={props.donationsArr[1]}></Trow>
-        <Trow obj={props.donationsArr[2]}></Trow>
+        {props.donationsArr.map(function (donation) {
+          return <Trow obj={donation}></Trow>;
+        })}
       </table>
       <div class="footer-container ">
         <Button text="See More..."></Button>
--- finalproj/src/Components/Dashboard/MainDashboard/BCRight/PieChartCont/LabelArea/LabelArea.js
+++ finalproj/src/Components/Dashboard/MainDashboard/BCRight/PieChartCont/LabelArea/LabelArea.js
@@ -1,28 +1,23 @@
 import "./LabelArea.css";
 import Label from "./Label/Label";
 function LabelArea() {
+  const labels = [
+    { category: "Hachnasas Kallah", amt: "$8,000.00", index: "first" },
+    { category: "Helping the Poor", amt: "$2,130.00", index: "second" },
+    { category: "Medical Institutions", amt: "$1,510.00", index: "third" },
+    { category: "Torah Institutions", amt: "$2,245.00", index: "fourth" },
+  ];
   return (
     <div class="pie-chart__labels">
-      <Label
-        labelColorClass="label__color first"
-        category="Hachnasas Kallah"
-        amt="$8,000.00"
-      ></Label>
-      <Label
-        labelColorClass="label__color second"
-        category="Helping the Poor"
-        amt="$2,130.00"
-      ></Label>
-      <Label
-        labelColorClass="label__color third"
-        category="Medical Institutions"
-        amt="$1,510.00"
-      ></Label>
-      <Label
-        labelColorClass="label__color fourth"
-        category="Torah Institutions"
-        amt="$2,245.00"
-      ></Label>
+      {labels.map(function (label) {
+        return (
+          <Label
+            labelColorClass={`label__color ${label.index}`}
+            category={label.category}
+            amt={label.amt}
+          ></Label>
+        );
+      })}
     </div>
   );
 }
