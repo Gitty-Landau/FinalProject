62e5046a477eb61cafe5ad0e06353d9b5fffdeac
Gitty Landau <GittyLandau04@gmail.com>
2023-06-21 19:53:31 +0300

    added income and donations pages

--- finalproj/src/Components/Dashboard/Dashboard.js
+++ finalproj/src/Components/Dashboard/Dashboard.js
@@ -1,8 +1,14 @@
 import "./Dashboard.css";
 import Header from "./Header/Header";
-
+import MainDashBoard from "./MainAreas/MainDashboard/MainDashboard";
+import Inbox from "./MainAreas/Inbox/Inbox";
+import Donations from "./MainAreas/Donations/Donations";
 function Dashboard(props) {
-  const ComponentArray = [<Dashboard></Dashboard>];
+  const ComponentArray = [
+    <MainDashBoard></MainDashBoard>,
+    <Inbox></Inbox>,
+    <Donations></Donations>,
+  ];
   return (
     <main class="main-content">
       <Header></Header>
